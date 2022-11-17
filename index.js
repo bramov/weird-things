b32da347ec6ef3a0f036eeca140aad7cd9c44753
ALEXEY ABRAMOV <alexey.abramov96@gmail.com>
2022-11-17 21:54:45 +0400

    fix: add validation

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const token = process.env.TELEGRAM_KEY;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
-  if (msg.text.includes('странно')) {
+  if (msg?.text && msg.text.includes('странно')) {
     await bot.sendMessage(msg.chat.id, 'Под словом `странно` подразумевается оценочное суждение. Данное слово не пытается никого оскорбить или задеть.')
   }
 });
